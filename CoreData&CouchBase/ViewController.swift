27cfc60ec5a03ce0aab19ca19d0dfe5fe773b124
AchillesSium <h_aqib11@rocketmail.com>
2017-12-23 13:47:48 +0600

    Outlet Taken

--- CoreData&CouchBase/ViewController.swift
+++ CoreData&CouchBase/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import CoreData
+import CouchbaseLite
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var uniqueIDTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var saveButtonOutlet: UIButton!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var searchButtonOutlet: UIButton!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    var database: CBLDatabase!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
